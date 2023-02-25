85503bc1c6318b8370677dc857fe28e2c8dceed8
TINGCYGF <1127163160@qq.com>
2023-02-25 15:50:08 +0800

    :sparkles: feat(封装): axios

---
+++ packages/request/src/axios/helper.ts
@@ -0,0 +1,49 @@
+import { isObject, isString } from '@common/utils'
+import { Recordable } from './types'
+
+const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm'
+
+export function joinTimestamp<T extends boolean>(
+  join: boolean,
+  restful: T
+): T extends true ? string : object
+
+export function joinTimestamp(join: boolean, restful = false): string | object {
+  if (!join) {
+    return restful ? '' : {}
+  }
+  const now = new Date().getTime()
+  if (restful) {
+    return `?_t=${now}`
+  }
+  return { _t: now }
+}
+
+/**
+ * @description: Format request parameter time
+ */
+export function formatRequestDate(params: Recordable) {
+  if (Object.prototype.toString.call(params) !== '[object Object]') {
+    return
+  }
+
+  for (const key in params) {
+    const format = params[key]?.format ?? null
+    if (format && typeof format === 'function') {
+      params[key] = params[key].format(DATE_TIME_FORMAT)
+    }
+    if (isString(key)) {
+      const value = params[key]
+      if (value) {
+        try {
+          params[key] = isString(value) ? value.trim() : value
+        } catch (error) {
+          throw new Error(error as any)
+        }
+      }
+    }
+    if (isObject(params[key])) {
+      formatRequestDate(params[key])
+    }
+  }
+}
