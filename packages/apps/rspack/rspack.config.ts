da42c1b51316d734d0bf21fca48e23a40691e2f3
TING <1127163160@qq.com>
2023-03-27 20:43:44 +0800

    :package: chore(配置): rspack config

--- packages/apps/rspack/rspack.config.ts
+++ packages/apps/rspack/rspack.config.ts
@@ -2,25 +2,39 @@
  * @type {import('@rspack/cli').Configuration}
  */
 import { Configuration } from '@rspack/cli'
+import * as path from 'path'
 
-export const config: Configuration = {
-  context: __dirname,
-  entry: {
-    main: './src/main.jsx'
-  },
-  builtins: {
-    html: [
-      {
-        template: './index.html'
-      }
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.svg$/,
-        type: 'asset'
-      }
-    ]
+const config = (env, argv): Configuration => {
+  console.log('env配置项', env, argv)
+  return {
+    context: __dirname,
+    entry: {
+      main: './src/main.jsx'
+    },
+    output: {
+      publicPath: '/assets/',
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+      chunkFilename: 'js/[name]-[hash].js',
+      cssChunkFilename: 'css/[name]-[hash].css',
+      assetModuleFilename: '[ext]/[name][ext]'
+    },
+    builtins: {
+      html: [
+        {
+          template: './index.html'
+        }
+      ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          type: 'asset/resource'
+        }
+      ]
+    }
   }
 }
+
+export = config
