2318fd67dcb096e441d7457a10ffa0a2cb0aaf31
TING <1127163160@qq.com>
2023-04-04 18:25:30 +0800

    :sparkles: feat(framework): add error tip test

--- packages/cli/create/test/cli.test.ts
+++ packages/cli/create/test/cli.test.ts
@@ -1,7 +1,17 @@
 import { expect, test } from 'vitest'
+import { execa } from 'execa'
 
-test('wrong agent', () => {
+const bin = () => {
+  return (...args: any) => execa('esno ./src/cli.ts', args)
+}
+
+test('vitest test', () => {
   expect(() => {
     throw new Error('vitest test')
   }).toThrow('vitest test')
+})
+
+test('run error command', async () => {
+  const { stderr } = await bin()('iii')
+  expect(stderr).toContain('未知的命令：iii')
 })
