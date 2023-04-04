4d008604247915d0fde4dbda8a58bf7f48e6b471
TING <1127163160@qq.com>
2023-04-04 12:21:19 +0800

    :bug: fix(framework): remove cli fix

---
+++ packages/cli/utils/src/pkg.ts
@@ -0,0 +1,17 @@
+import { execa } from 'execa'
+
+type WorkspacePackage = { name: string; version?: string; path: string }
+// 获取所有子项目的信息 as WorkspacePackage （筛选需要打包的子项目）
+async function getAllPackages(namespace?: string) {
+  const { stdout } = await execa('pnpm', ['ls', '-r', '--depth', '-1', '--json'])
+  return (JSON.parse(stdout) as WorkspacePackage[]).filter((p) => {
+    return p.name.includes(namespace) || !namespace
+  })
+}
+
+async function getPackage() {
+  const { stdout } = await execa('npm', ['ls', '-r', '--depth', '-1', '--json'])
+  return JSON.parse(stdout) as WorkspacePackage
+}
+
+export { getPackage, getAllPackages }
