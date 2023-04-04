27cf135cf9c6f3b1e1f912fb4f215f4370affd77
TING <1127163160@qq.com>
2023-04-04 12:45:10 +0800

    :sparkles: feat(framework): unbuild cli

---
+++ packages/cli/create/build.config.ts
@@ -0,0 +1,10 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: ['src/index', 'src/cli'],
+  rollup: {
+    inlineDependencies: true
+  },
+  clean: true,
+  declaration: true
+})
