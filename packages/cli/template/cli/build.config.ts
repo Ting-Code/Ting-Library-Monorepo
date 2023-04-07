4f412987d1face3ed47f5667fd867982b67840d4
TING <1127163160@qq.com>
2023-04-07 23:06:08 +0800

    :sparkles: feat(framework): cli template

--- packages/cli/template/cli/build.config.ts
+++ packages/cli/template/cli/build.config.ts
@@ -2,7 +2,7 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-  entries: ['src/index', 'src/pkg', 'src/log'],
+  entries: ['src/index', 'src/cli'],
   rollup: {
     inlineDependencies: true
   },
--- packages/cli/utils/src/log.ts
+++ packages/cli/utils/src/log.ts
@@ -1,4 +1,3 @@
-// @ts-ignore
 import log from 'npmlog'
 
 export function isDebug() {
