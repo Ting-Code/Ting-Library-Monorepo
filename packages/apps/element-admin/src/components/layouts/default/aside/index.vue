adbb0b6933add327156f4eea9b6bc1effd811c20
TINGCYGF <1127163160@qq.com>
2023-06-20 10:32:05 +0800

    :sparkles: feat(business): add layout aside

---
+++ packages/apps/element-admin/src/components/layouts/default/aside/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <el-aside>777777777777777777</el-aside>
+</template>
+
+<script setup lang="ts">
+  defineOptions({
+    name: 'LayoutAside'
+  })
+</script>
--- packages/apps/element-admin/src/components/layouts/default/index.vue
+++ packages/apps/element-admin/src/components/layouts/default/index.vue
@@ -1,30 +1,38 @@
 <template>
   <el-container :class="ns.b()">
     <LayoutFeature />
-    <LayoutHeader fixed />
+    <LayoutHeader fixed v-if="false" />
     <el-container>
-      <el-main>
-        <main> main </main>
-      </el-main>
-      <el-footer> footer {{ width }} </el-footer>
+      <el-drawer v-if="isMobile">
+        <LayoutAside />
+      </el-drawer>
+      <LayoutAside v-else />
+      <el-container>
+        <el-main>
+          <main> main </main>
+        </el-main>
+        <el-footer> footer {{ width }} </el-footer>
+      </el-container>
     </el-container>
   </el-container>
 </template>
 <script setup lang="ts">
   import LayoutFeature from './feature/index.vue'
+  import LayoutAside from '@/components/layouts/default/aside/index.vue'
   import LayoutHeader from './herder/index.vue'
   import { useNamespace } from '@/hooks/useNamespace'
   import { useAppProviderContext } from '@/components/application/useAppContext'
+  import { toRefs } from 'vue'
 
+  defineOptions({
+    name: 'DefaultLayout'
+  })
   const ns = useNamespace('default-layout')
-  const { width } = useAppProviderContext()
-  setTimeout(() => {
-    console.log(width)
-  }, 3000)
+  const { width, isMobile } = toRefs(useAppProviderContext())
 </script>
 
 <style lang="scss">
   @include b(default-layout) {
-    height: 100%;
+    min-height: 100%;
   }
 </style>
