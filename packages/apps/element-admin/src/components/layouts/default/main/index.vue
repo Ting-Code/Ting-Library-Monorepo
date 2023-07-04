f0f12f248ed1279de7b0dab4605eae643e3280b3
Ting <1127163160@qq.com>
2023-07-04 15:46:53 +0800

    :sparkles: feat(business): layout main

--- packages/apps/element-admin/src/components/layouts/default/main/index.vue
+++ packages/apps/element-admin/src/components/layouts/default/main/index.vue
@@ -1,19 +1,37 @@
 <template>
-  <el-main :class="ns.b()">
-    <router-view />
-  </el-main>
+  <main :class="ns.b()">
+    <router-view>
+      <template #default="{ Component, route }">
+        <transition name="fade-slide" mode="out-in" appear>
+          <keep-alive v-if="keepAliveComponents.length" :include="keepAliveComponents">
+            <component :is="Component" :key="route.fullPath" />
+          </keep-alive>
+          <component v-else :is="Component" :key="route.fullPath" />
+        </transition>
+      </template>
+    </router-view>
+  </main>
 </template>
 
 <script lang="ts" setup>
   import { useNamespace } from '@/hooks/useNamespace'
+  import { useAsyncRouteStoreWidthOut } from '@/store/modules/asyncRoute'
+  import { computed } from 'vue'
+  const asyncRouteStore = useAsyncRouteStoreWidthOut()
 
   defineOptions({ name: 'LayoutMain' })
+
+  const keepAliveComponents = computed(() => asyncRouteStore.keepAliveComponents)
+  setTimeout(() => {
+    console.log(asyncRouteStore, asyncRouteStore.keepAliveComponents, keepAliveComponents)
+  }, 5000)
 
   const ns = useNamespace('layout-main')
 </script>
 
 <style lang="scss">
   @include b(layout-main) {
+    height: 100%;
     background-color: getCssVar('bg-color', 'main');
   }
 </style>
