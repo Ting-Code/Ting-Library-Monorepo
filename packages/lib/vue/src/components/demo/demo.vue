cb641cc4b260193df65e3c92dfa566973f1b16d9
TING <1127163160@qq.com>
2024-05-06 21:45:13 +0800

    :bug: fix(architecture): typescript tes check

--- packages/lib/vue/src/components/demo/demo.vue
+++ packages/lib/vue/src/components/demo/demo.vue
@@ -1,14 +1,19 @@
 <template>
   <ElButton type="primary"> {{ props.title + props.name || '' }}</ElButton>
+  <ElDrawer title="asd" />
 </template>
 
 <script setup lang="ts">
-  import { ElButton } from 'element-plus'
+  import { ElButton, ElDrawer } from 'element-plus'
+
   defineOptions({
     name: 'Demo'
   })
-  const props = defineProps<{
-    title: string
-    name?: string
-  }>()
+  const props = withDefaults(
+    defineProps<{
+      title: string
+      name?: string
+    }>(),
+    {}
+  )
 </script>
