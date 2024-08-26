fd53cb6c01a3b7c2a56379a109f70df771c1ab60
Ting <1127163160@qq.com>
2024-08-26 19:37:53 +0800

    :tada: version(architecture): v0.3

--- packages/apps/admin/src/global.d.ts
+++ packages/apps/admin/src/global.d.ts
@@ -17,10 +17,10 @@ declare global {
     }
     lastBuildTime: string
   }
-  // declare interface Window {
-  //   // Global vue app instance
-  //   __APP__: App<Element>;
-  // }
+  declare interface Window {
+    // Global vue app instance
+    __isOneRouter__: boolean
+  }
 
   // vue
   declare type PropType<T> = VuePropType<T>
--- packages/apps/admin/src/views/layouts/parentLayout/index.vue
+++ packages/apps/admin/src/views/layouts/parentLayout/index.vue
@@ -1,3 +1,12 @@
 <template>
   <router-view />
 </template>
+
+<script lang="ts" setup>
+  import { emitMittRouter } from '@tingcode/system'
+
+  if (!window.__isOneRouter__) {
+    window.__isOneRouter__ = true
+    emitMittRouter(window.location.href)
+  }
+</script>
