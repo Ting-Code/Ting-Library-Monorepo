abc1923ee463c1e264ffb639d2246c7531e4af6e
Ting <1127163160@qq.com>
2025-03-05 10:44:03 +0800

    :package: chore(config): docs build

--- packages/apps/admin/src/components.d.ts
+++ packages/apps/admin/src/components.d.ts
@@ -17,6 +17,7 @@ declare module 'vue' {
     ElDropdown: typeof import('element-plus/es')['ElDropdown']
     ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
+    ElEmpty: typeof import('element-plus/es')['ElEmpty']
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElMain: typeof import('element-plus/es')['ElMain']
