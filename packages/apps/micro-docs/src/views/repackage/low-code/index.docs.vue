bd051877925aeb9f722f5e42a6c95d6db933aad5
Ting <1127163160@qq.com>
2025-04-18 11:35:48 +0800

    :sparkles: feat(business): low code

--- packages/apps/micro-docs/src/views/repackage/low-code/index.docs.vue
+++ packages/apps/micro-docs/src/views/repackage/low-code/index.docs.vue
@@ -1,11 +1,47 @@
 <template>
-  <DocsLayout>
-    <Markdown>
-      <pre>
-# 🧱低代码
-      </pre>
-    </Markdown>
-  </DocsLayout>
+  <div :class="ns.b()">
+    <div :class="ns.e('head')">
+      <template v-if="isMobile">
+        <ReButton v-show="!isShowStencil" @click="isShowStencil = true" />
+        <ReButton v-show="isShowStencil" @click="isShowStencil = false" />
+      </template>
+      <ReButton :icon="Share" />
+      <ReButton :icon="CaretLeft" />
+      <ReButton :icon="CaretRight" />
+      <ReButton :icon="Delete" />
+      <ReButton :icon="ZoomIn" />
+      <ReButton :icon="ZoomOut" />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ReButton } from '@tingcode/lib-vue'
+  import { Delete, ZoomIn, ZoomOut, CaretLeft, CaretRight, Share } from '@element-plus/icons-vue'
+  import { useNamespace } from '@tingcode/system'
+  import { useAppProviderContext } from '@/application/useAppContext'
+  const { isMobile } = toRefs(useAppProviderContext())
+  const isShowStencil = ref(true)
+  const ns = useNamespace('low-code')
+  watch(
+    isMobile,
+    (val) => {
+      if (val) {
+        isShowStencil.value = false
+      } else {
+        isShowStencil.value = true
+      }
+    },
+    { immediate: true }
+  )
+</script>
+
+<style lang="scss" scoped>
+  @include b(low-code) {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
