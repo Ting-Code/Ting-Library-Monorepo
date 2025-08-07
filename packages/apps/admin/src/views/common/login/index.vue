07ef1593f0cf267392bc7ded93bcf50908fa36d2
Ting <1127163160@qq.com>
2025-08-07 16:28:57 +0800

    :tada: version(architecture): v1.0

--- packages/apps/admin/src/views/common/login/index.vue
+++ packages/apps/admin/src/views/common/login/index.vue
@@ -13,7 +13,7 @@
         :star-speed="0.2"
       />
     </el-main>
-    <div :class="ns.e('left')">
+    <div :class="ns.e('left')" v-if="!isMobile">
       <TextType
         :text="['Welcome to Ting Library', '欢迎来到Ting知识库']"
         :typingSpeed="80"
@@ -27,13 +27,31 @@
       </div>
       <ShinyText text="快速开始" />
     </div>
-    <div :class="ns.e('right')">
+    <div :class="ns.e('right')" v-if="!isMobile">
+      <CardSwap />
+    </div>
+    <div :class="ns.e('top')" v-if="isMobile">
+      <TextType
+        :text="['Welcome to Ting Library', '欢迎来到Ting知识库']"
+        :typingSpeed="80"
+        :pauseDuration="1600"
+        :showCursor="true"
+        cursorCharacter="_"
+        :className="ns.em('top', 'title')"
+      />
+      <div :class="ns.em('top', 'text')">
+        基于 Monorepo 搭建的架构与文档于一体的知识库，沉淀技术解决方案并提供直达源码的直观文档。
+      </div>
+      <ShinyText text="快速开始" />
+    </div>
+    <div :class="ns.e('bottom')" v-if="isMobile">
       <CardSwap />
     </div>
   </el-container>
 </template>
 
 <script setup lang="ts">
+  import { useAppProviderContext } from '@/views/application/useAppContext'
   import Galaxy from './components/galaxy/index.vue'
   import TextType from './components/text-type/index.vue'
   import ShinyText from './components/start-button/index.vue'
@@ -44,7 +62,7 @@
   defineOptions({
     name: 'Login'
   })
-
+  const { isMobile } = toRefs(useAppProviderContext())
   const ns = useNamespace('login')
 </script>
 
@@ -69,8 +87,8 @@
 
     @include e(left) {
       position: absolute;
-      top: calc(32vh + 100px);
-      left: calc(12vh + 100px);
+      top: calc(50vh - 100px);
+      left: calc(8vw + 20px);
       @include m(title) {
         font-size: calc(1vw + 30px);
         font-weight: bold;
@@ -95,6 +113,42 @@
       height: 100px;
       top: calc(38vh + 200px);
       right: calc(12vw + 100px);
+
+      @include min-XXL {
+        top: calc(38vh + 300px);
+        right: calc(12vw + 100px);
+      }
+    }
+    @include e(top) {
+      position: absolute;
+      width: 100vw;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      top: calc(20vh + 20px);
+      @include m(title) {
+        font-size: calc(1vw + 30px);
+        font-weight: bold;
+        color: getCssVar('text-color', 'primary');
+        padding-bottom: 20px;
+      }
+      @include m(text) {
+        font-size: calc(0.5vw + 8px);
+        padding-bottom: 38px;
+        color: #a7ef9e;
+        max-width: calc(50vw);
+        text-align: center;
+        text-shadow:
+          0 0 2px rgba(255, 255, 255, 0.1),
+          0 0 4px rgba(255, 255, 255, 0.3),
+          0 0 8px rgba(255, 255, 255, 0.4),
+          0 0 136px rgba(60, 255, 79, 0.8);
+      }
+    }
+    @include e(bottom) {
+      position: absolute;
+      top: calc(80vh + 200px);
 
       @include min-XXL {
         top: calc(38vh + 300px);
